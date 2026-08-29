4a8402fe90b19d22ea472acb7549beb0b04f06b0
maitryp41-ctrl <maitryp41@gmail.com>
2026-08-30 01:06:22 +0530

    Refactor endpoint URL to use BASE_URL constant
    Updated endpoint URL definition to use a constant.

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ agent:
   authtoken: 3IbFivxzFJ2dcrwAZU8t1jw52tN_3NDCVTaX8kAYP8swiUa19
 endpoints:
   - name: your_endpoint_name
-    url: https://uselessly-monitor-lagging.ngrok-free.dev
+    const BASE_URL = "https://uselessly-monitor-lagging.ngrok-free.dev";
     upstream:
       url: 80
