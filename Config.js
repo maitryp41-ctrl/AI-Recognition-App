18e93c134d8ceefac6d90fa08df12e7b40cfc40e
maitryp41-ctrl <maitryp41@gmail.com>
2026-08-30 00:28:41 +0530

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,8 @@
+version: 3
+agent:
+  authtoken: 3IbFivxzFJ2dcrwAZU8t1jw52tN_3NDCVTaX8kAYP8swiUa19
+endpoints:
+  - name: your_endpoint_name
+    url: https://uselessly-monitor-lagging.ngrok-free.dev
+    upstream:
+      url: 80
