322bf6d09c8c521c4198b24802143d44e5908625
maitryp41-ctrl <maitryp41@gmail.com>
2026-08-30 03:12:16 +0530

    Fix backendUrl casing in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 
    const BASE_URL = "https://uselessly-monitor-lagging.ngrok-free.dev";
 const CONFIG = {
-  backendUrl: "https://uselessly-monitor-lagging.ngrok-free.app" // 👈 no :5000
+  backendurl: "https://uselessly-monitor-lagging.ngrok-free.app" // 👈 no :5000
 };
